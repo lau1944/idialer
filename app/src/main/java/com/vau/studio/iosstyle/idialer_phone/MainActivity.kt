b488b4b81fc6f2893eb0f83fbb3f8b67fa4fd1aa
lau1944 <a954723219@gmail.com>
2021-08-23 19:13:00 +0800

    add nav bar

--- app/src/main/java/com/vau/studio/iosstyle/idialer_phone/MainActivity.kt
+++ app/src/main/java/com/vau/studio/iosstyle/idialer_phone/MainActivity.kt
@@ -3,9 +3,6 @@ package com.vau.studio.iosstyle.idialer_phone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.vau.studio.iosstyle.idialer_phone.views.composable.AppTheme
 import com.vau.studio.iosstyle.idialer_phone.views.composable.home_screen.HomeScreen
 
