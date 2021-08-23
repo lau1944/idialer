1ee775476ea15c94a4d8bf24a4aa450375a98193
liuquan <lauchuen94@gmail.com>
2021-08-23 11:35:09 +0800

    app model

--- app/src/main/java/com/vau/studio/iosstyle/idialer_phone/MainActivity.kt
+++ app/src/main/java/com/vau/studio/iosstyle/idialer_phone/MainActivity.kt
@@ -3,19 +3,34 @@ package com.vau.studio.iosstyle.idialer_phone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { Test() }
+
+        val clickCount = mutableStateOf(0)
+        setContent {
+            MaterialTheme() {
+                Test(clickCount.value, onClick = {
+                    clickCount.value++
+                })
+            }
+        }
     }
 }
 
-@Preview
 @Composable
-fun Test() {
-    Text("Hello World")
+fun Test(clickCount: Int, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text("Button On clicked $clickCount times")
+    }
 }
