c045a4e9918df773e9c68b8340cddcd92ad0cc27
liuquan <lauchuen94@gmail.com>
2021-09-07 22:52:01 +0800

    feat: contact query

--- app/src/main/java/com/vau/studio/iosstyle/idialer_phone/data/models/CallHistory.kt
+++ app/src/main/java/com/vau/studio/iosstyle/idialer_phone/data/models/CallHistory.kt
@@ -3,9 +3,10 @@ package com.vau.studio.iosstyle.idialer_phone.data.models
 const val INCOMING_CALL : String = "INCOMING"
 const val OUTGOING_CALL : String = "OUTGOING"
 
-data class CallLog(
+data class CallHistory(
     val name: String? = "",
     val number: String? = "",
     val date: String? = "",
-    val type: String? = INCOMING_CALL
+    val type: String? = INCOMING_CALL,
+    val duration: String? = ""
 )
--- app/src/main/java/com/vau/studio/iosstyle/idialer_phone/data/repositories/PhoneRepository.kt
+++ app/src/main/java/com/vau/studio/iosstyle/idialer_phone/data/repositories/PhoneRepository.kt
@@ -1,8 +1,16 @@
 package com.vau.studio.iosstyle.idialer_phone.data.repositories
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.provider.CallLog
 import android.provider.ContactsContract
+import androidx.annotation.RequiresApi
 import androidx.core.database.getStringOrNull
+import com.vau.studio.iosstyle.idialer_phone.data.models.CallHistory
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -15,7 +23,10 @@ object PhoneRepository {
     /**
      * Get all contacts
      */
-    fun getContactNames(context: Context, lookUp: String?) : Flow<List<String>?> = flow {
+    fun getContactNames(
+        @ApplicationContext context: Context,
+        lookUp: String?
+    ): Flow<List<String>?> = flow {
         withContext(Dispatchers.IO) {
             val cursor = context.contentResolver.query(
                 ContactsContract.Data.CONTENT_URI,
@@ -37,10 +48,51 @@ object PhoneRepository {
                         contacts.add(cursor.getStringOrNull(nameIndex) ?: "")
                     }
                 }
+                cursor.close()
+
                 withContext(Dispatchers.Default) {
                     emit(contacts)
                 }
+            }
+        }
+    }
+
+    /**
+     * Get call logs
+     */
+    @SuppressLint("Recycle")
+    fun getCallLog(
+        @ApplicationContext context: Context,
+        projection: Array<String>
+    ): Flow<List<CallHistory>> = flow {
+        withContext(Dispatchers.IO) {
+            val cursor =
+                context.contentResolver.query(
+                    CallLog.Calls.CONTENT_URI,
+                    null, null, null, CallLog.Calls.DATE + " DESC"
+                )
+            if (cursor != null) {
+                val callLogs = mutableListOf<CallHistory>()
+                val number = cursor.getColumnIndex(CallLog.Calls.NUMBER)
+                val name = cursor.getColumnIndex(CallLog.Calls.CACHED_NAME)
+                val type = cursor.getColumnIndex(CallLog.Calls.TYPE)
+                val date = cursor.getColumnIndex(CallLog.Calls.DATE)
+                val duration = cursor.getColumnIndex(CallLog.Calls.DURATION)
+                while (cursor.moveToNext()) {
+                    val callHistory = CallHistory(
+                        name = cursor.getString(name),
+                        number = cursor.getString(number),
+                        type = cursor.getString(type),
+                        date = cursor.getString(date),
+                        duration = cursor.getString(duration)
+                    )
+                    callLogs.add(callHistory)
+                }
                 cursor.close()
+
+                withContext(Dispatchers.Default) {
+                    emit(callLogs)
+                }
             }
         }
     }
